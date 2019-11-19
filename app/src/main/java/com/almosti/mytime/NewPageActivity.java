c910910f6505e17f95ac998f34ce171ea5020175
almosti <almosti@users.noreply.github.com>
2019-11-19 18:00:50 +0800

    Initial commit

---
+++ app/src/main/java/com/almosti/mytime/NewPageActivity.java
@@ -0,0 +1,13 @@
+package com.almosti.mytime;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class NewPageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_page);
+    }
+}
