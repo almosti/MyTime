08fbe15dc039ce42a4ac1a563de27a03080832ac
almosti <almosti@users.noreply.github.com>
2019-11-25 21:18:18 +0800

    框架1

--- app/src/main/java/com/almosti/mytime/MainActivity.java
+++ app/src/main/java/com/almosti/mytime/MainActivity.java
@@ -1,8 +1,20 @@
 package com.almosti.mytime;
 
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.text.Spannable;
+import android.text.SpannableStringBuilder;
+import android.text.style.ForegroundColorSpan;
+import android.text.style.RelativeSizeSpan;
+import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -12,10 +24,93 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import java.sql.Time;
+import java.util.ArrayList;
+import java.util.Calendar;
+
 public class MainActivity extends AppCompatActivity
-        implements NavigationView.OnNavigationItemSelectedListener {
+        implements NavigationView.OnNavigationItemSelectedListener,listview_fragment.callbackvalue {
+
+    /*定义全局变量*/
+    private ArrayList<TimePage> TimeList;
+    ListViewAdapter theListAdapter;
+    private static final int REQUEST_CODE_NEW_PAGE=10;
+
+    /*定义listviewadapter
+     * 重写其中的add，remove，getcount，getitem，removeitem，makeitemview，getview方法*/
+    public class ListViewAdapter extends BaseAdapter {
+        ArrayList<View> itemViews;
+
+        public ListViewAdapter(ArrayList<TimePage> TimeList){
+            itemViews = new ArrayList<View>(TimeList.size());
+
+            //初始化列表
+            for (int i=0; i<TimeList.size(); ++i){
+                itemViews.add(makeItemView(TimeList.get(i)));
+            }
+        }
+
+        public void addItem(TimePage page) {
+            TimeList.add(page);
+            View view=makeItemView(page);
+            itemViews.add(view);
+
+        }
+
+        public void removeItem(int positon){
+            itemViews.remove(positon);
+            TimeList.remove(positon);
+        }
+
+        public int getCount() {
+            return itemViews.size();
+        }
+
+        public View getItem(int position) {
+            return itemViews.get(position);
+        }
+
+        public long getItemId(int position) {
+            return position;
+        }
+
+        //重写listview显示方式
+        private View makeItemView(TimePage page) {
+
+            LayoutInflater inflater = (LayoutInflater)MainActivity.this
+                    .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+
+            // 使用View的对象itemView与R.layout.item关联
+            View itemView = inflater.inflate(R.layout.time_list_item, null);
+
+            // 通过findViewById()方法实例R.layout.item内各组件
+            TextView dateText = (TextView) itemView.findViewById(R.id.timeDistance);
+            int timeDistance=page.getTimeDistance().get(Calendar.DATE);
+            String timeDistanceString;
+            if(page.getTimeDistanceSign()){
+                timeDistanceString="还剩\n"+timeDistance+"天";
+            }else{
+                timeDistanceString="已经\n"+(-timeDistance)+"天";
+            }
+            dateText.setText(timeDistanceString);
+
+            //在有内容的页面显示中要获取行数并只显示最多三行
+            TextView contentText = (TextView) itemView.findViewById(R.id.timeContent);
+            String contentString=page.getTitle()+"\n"+page.getYear()+"年"+page.getMonth()+"月"+page.getDay()+"日";
+            contentText.setText(contentString);
+            return itemView;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            return itemViews.get(position);
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,8 +130,27 @@ public class MainActivity extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+
+        //初始化计时器数据列表
+        if(TimeList==null){
+            TimeList=new ArrayList<>();
+        }
+        theListAdapter=new ListViewAdapter(TimeList);
+        final listview_fragment fragment = new listview_fragment(theListAdapter);
+        FragmentManager fragmentManager=getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction=fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.frame_container,fragment);
+        fragmentTransaction.commit();
     }
 
+    /*实现回调函数
+     * 通过这个函数获取listview点击的项目*/
+    @Override
+    public void sendvalue(int selecteditem) {
+        TimePage page = TimeList.get(selecteditem);
+    }
+
+    //返回键优先关闭左侧滑动菜单
     @Override
     public void onBackPressed() {
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
@@ -47,7 +161,7 @@ public class MainActivity extends AppCompatActivity
         }
     }
 
-    //去除了右上角设置菜单
+    //去除右上角设置菜单
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
@@ -70,7 +184,28 @@ public class MainActivity extends AppCompatActivity
         return super.onOptionsItemSelected(item);
     }
 
-    @SuppressWarnings("StatementWithEmptyBody")
+    /*创建activity的返回值*/
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        switch (requestCode){
+            case REQUEST_CODE_NEW_PAGE:
+                if (resultCode == RESULT_OK){
+                    Bundle bundle=data.getExtras();
+                    TimePage page;
+                    if(bundle.getSerializable("TimePage")!=null){
+                        page = (TimePage) bundle.getSerializable("TimePage");
+                    }else{
+                        Toast.makeText(MainActivity.this, "添加新倒计时产生错误！", Toast.LENGTH_LONG).show();
+                        return;
+                    }
+                    theListAdapter.addItem(page);
+                    theListAdapter.notifyDataSetChanged();
+                }
+                break;
+        }
+    }
+
     @Override
     public boolean onNavigationItemSelected(MenuItem item) {
         // Handle navigation view item clicks here.
@@ -95,9 +230,15 @@ public class MainActivity extends AppCompatActivity
         return true;
     }
 
+    //添加新计时器项
     class AddNewPageListener implements View.OnClickListener{
         public void onClick(View view) {
-            Toast.makeText(MainActivity.this,"!",Toast.LENGTH_LONG).show();
+            TimePage page=new TimePage();
+            Intent intent =new Intent(MainActivity.this,NewPageActivity.class);
+            Bundle bundle = new Bundle();
+            bundle.putSerializable("TimePage",page);
+            intent.putExtras(bundle);
+            startActivityForResult(intent,REQUEST_CODE_NEW_PAGE);
         }
     }
 }
---
+++ app/src/main/java/com/almosti/mytime/TimePage.java
@@ -0,0 +1,138 @@
+package com.almosti.mytime;
+
+import java.io.Serializable;
+import java.util.Calendar;
+
+public class TimePage implements Serializable {
+
+    //用于防止页面在写数据过程中出错导致的变量未正确初始化
+    public TimePage() {
+        title="";
+        remark="";
+        pictureID=-1;
+        year=-1;
+        month=-1;
+        day=-1;
+        hour=-1;
+        minute=-1;
+        cycle=-1;
+        second=-1;
+    }
+
+    //用于获取剩余时间
+    Calendar getTimeDistance(){
+        Calendar calendar=Calendar.getInstance();
+        calendar.set(year-calendar.get(Calendar.YEAR),
+                month-calendar.get(Calendar.MONTH),
+                day-calendar.get(Calendar.DATE),
+                hour-calendar.get(Calendar.HOUR_OF_DAY),
+                minute-calendar.get(Calendar.MINUTE),
+                second-calendar.get(Calendar.SECOND));
+        return calendar;
+    }
+
+    //返回倒计时是否已过
+    boolean getTimeDistanceSign(){
+        Calendar calendar=Calendar.getInstance();
+        calendar=getTimeDistance();
+        if(calendar.get(Calendar.YEAR)<0)
+            return false;
+        else if(calendar.get(Calendar.MONTH)<0)
+            return false;
+        else if(calendar.get(Calendar.DATE)<0)
+            return false;
+        else if(calendar.get(Calendar.HOUR_OF_DAY)<0)
+            return false;
+        else if(calendar.get(Calendar.MINUTE)<0)
+            return false;
+        else if(calendar.get(Calendar.SECOND)<0)
+            return false;
+        return true;
+    }
+
+    public String getTitle() { return title; }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getRemark() {
+        return remark;
+    }
+
+    public void setRemark(String remark) {
+        this.remark = remark;
+    }
+
+    public int getPictureID() {
+        return pictureID;
+    }
+
+    public void setPictureID(int pictureID) {
+        this.pictureID = pictureID;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public int getMonth() {
+        return month;
+    }
+
+    public void setMonth(int month) {
+        this.month = month;
+    }
+
+    public int getDay() {
+        return day;
+    }
+
+    public void setDay(int day) {
+        this.day = day;
+    }
+
+    public int getHour() {
+        return hour;
+    }
+
+    public void setHour(int hour) {
+        this.hour = hour;
+    }
+
+    public int getMinute() {
+        return minute;
+    }
+
+    public void setMinute(int minute) {
+        this.minute = minute;
+    }
+
+    public int getCycle() {
+        return cycle;
+    }
+
+    public void setCycle(int cycle) {
+        this.cycle = cycle;
+    }
+
+    public int getSecond() { return second; }
+
+    public void setSecond(int second) { this.second = second; }
+
+    private String title;
+    private String remark;
+    private int pictureID;
+    private int year;
+    private int month;
+    private int day;
+    private int hour;
+    private int minute;
+    private int second;
+    private int cycle;
+
+}
